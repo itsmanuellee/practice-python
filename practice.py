13e33ae719a39f6d74194166f4762ed360cb72e4
Manuel_Lee <Manuel_Lee>
2017-11-16 01:01:26 -1200

    git pushing

---
+++ practice.py
@@ -0,0 +1,11 @@
+something = int(raw_input("enter any number ranging from 1 - 3=> "))
+if (something==1):
+    print ("one")
+elif(something==2):
+    print ("two")
+elif (something==3):
+    print ("three")
+elif (something >3):
+    print ("it's outta range")
+else:
+    print ("it's below range")
